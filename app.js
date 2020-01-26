f002509253d34424cb55da99e0ac73631a8a3f58
sharwarigothe <gothe.s@northeastern.edu>
2020-01-26 09:44:26 -0800

    Adding assignment 2

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+//Create connection with mysql
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'sharwari',
+    password : 'password',
+    database: 'UserDetails'
+});
+
+//Connect
+db.connect(() => {
+    // if(err)
+    //     {console.error(err);
+          
+    //     }else{
+    
+    // console.log('MySql Connected');
+    //     }
+});
+
+
+
+const userRoutes = require('./api/routes/user_details');
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+//Routes which should handle requests
+app.use('/v1/user', userRoutes);
+
+app.use((req, res, next) =>{
+    const error = new Error('Not Found');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+      
+    });
+
+});
+
+module.exports = app;
