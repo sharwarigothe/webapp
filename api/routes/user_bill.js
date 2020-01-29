b54251798b89df396dd1e85a5ee2bdb6c5776053
sharwarigothe <gothe.s@northeastern.edu>
2020-01-29 11:16:32 -0800

    changes in apis

--- api/routes/user_bill.js
+++ api/routes/user_bill.js
@@ -32,7 +32,7 @@ db.connect((error) =>{
 
 router.post("/",(req,res,next)=>{
 
-    //var myEnum = new Enum(['paid', 'due', 'past_due', 'no_payment_required']);
+    var myEnum = new Enum(['paid', 'due', 'past_due', 'no_payment_required']);
 
     var d = new Date();
     var n = d.getMilliseconds();
@@ -109,14 +109,27 @@ const bill={
                         if(Number(bill.amount_due< 0.01)|| (Number(bill.amount_due > 20000))){
                            res.status(400).json({error: "The amount due is either less than minimum amount or greater than the maximum amount"});
                        }
-                     
-                       db.query("INSERT INTO Bill (id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount, categories, paymentStatus) VALUES ('"+id+"','"+created_ts+"','"+updated_ts+"','"+uuid+"','"+bill.vendor+"','"+bill.bill_date+"','"+bill.due_date+"','"+bill.amount_due+"','"+stringObj+"','"+bill.paymentStatus+"')",function(error,ress){
+                       if(Number(bill.amount_due === undefined)){
+                        res.status(400).json({error: "amount due empty"});
+                       }
+                       else if(bill.paymentStatus === undefined){
+                        res.status(400).json({error: "paymentStatus empty"});
+                       }
+
+                    //    else if(bill.paymentStatus != "paid" || bill.paymentStatus != "due"){
+                    //     res.status(400).json({error: "payment status not paid or due"});
+                    //    }
+                       else{
+                       
+                       db.query("INSERT INTO Bill (id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount_due, categories, paymentStatus) VALUES ('"+id+"','"+created_ts+"','"+updated_ts+"','"+uuid+"','"+bill.vendor+"','"+bill.bill_date+"','"+bill.due_date+"','"+bill.amount_due+"','"+stringObj+"','"+bill.paymentStatus+"')",function(error,ress){
                         console.log(ress);
                         if(error){
                             console.log("error in insert");
                             throw error;
                         }
-                        if(!bill.vendor || !bill.bill_date || !bill.due_date || !bill.amount_due || !bill.categories || !bill.paymentStatus){
+                        
+
+                        else if(!bill.vendor || !bill.bill_date || !bill.due_date || !bill.amount_due || !bill.categories || !bill.paymentStatus){
                             res.status(400).json({error: "Please enter required fields"});
                         }
 
@@ -142,6 +155,7 @@ const bill={
                         });
                     }
                     });
+                }
                     }
                     else{
                         res.status(400).json({error: "Password does not match"});
@@ -184,12 +198,16 @@ router.get("/:id",function(req,res){
 
                 bcrypt.compare(password1, pa, (error, result) => {
                     if(result==true){
-                        db.query(`Select id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount, categories from Bill where id = "${id}"`,function (error,resulte){
+                        db.query(`Select id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount_due, categories, paymentStatus from Bill where id = "${id}"`,function (error,resulte){
                           
                             if(error){
                                 throw error;
                             }
-                            if(resulte[0].owner_id == results[0].id){  
+                            if(resulte == false){
+                                res.status(404).json({error: "bill id does not exist"});
+                                console.log("Wrong id");
+                            }
+                            else if(resulte[0].owner_id == results[0].id){  
                                 var cat = JSON.parse(resulte[0].categories);
                                 res.status(200).json({
                                     id: id,
@@ -200,19 +218,20 @@ router.get("/:id",function(req,res){
                                     bill_date: resulte[0].bill_date,
                                     due_date: resulte[0].due_date,
                                     amount_due: resulte[0].amount_due,
-                                    categories: cat
+                                    categories: cat,
+                                    paymentStatus: resulte[0].paymentStatus
                             
                                 })
                             }
                             else{
-                                res.status(400).json({error: "Cannot fetch other users bill"});
+                                res.status(401).json({error: "UNAUTHORIZED, Cannot fetch other users bill"});
                             }
                           
   
                         });
                     }
                     else{
-                        res.send(400).json({error: "User does not exist"});
+                        res.send(404).json({error: "User does not exist"});
                     }
                 });
             }
@@ -220,69 +239,8 @@ router.get("/:id",function(req,res){
     }
 });
 
-// router.get("/:id",function(req,res){
 
-//     var d = new Date();
-//     var n = d.getMilliseconds();
-
-//     const id = req.params.id;
-//     console.log(id);
-
-//     if (req.headers.authorization && req.headers.authorization.search('Basic ') === 0){
-//         var header = new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString();
-//         header = header.split(":");
-    
-//         var username1 = header[0];
-//         var password1 = header[1];
-        
-//         console.log(username1);
-//         console.log(password1);
-
-//         db.query( `select id, first_name, last_name, password, email_address, account_created, account_updated from user_details where email_address = "${username1}"`,function(error, results,row){
-//             if(error){
-//                 throw error;
-//             }
-//             else if(results.length >0){
-//                 var pa = results[0].password;
-//                 var uuid = results[0].id;
-
-//                 bcrypt.compare(password1, pa, (error, result) => {
-//                     if(result==true){
-//                         db.query(`Select id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount, categories from Bill where id = "${id}"`,function (error,resulte){
-                          
-//                             if(error){
-//                                 throw error;
-//                             }
-//                             if(resulte[0].owner_id == results[0].id){  
-//                                 var cat = JSON.parse(resulte[0].categories);
-//                                 res.status(200).json({
-//                                     id: id,
-//                                     created_ts: resulte[0].created_ts,
-//                                     updated_ts: resulte[0].updated_ts,
-//                                     owner_id: resulte[0].owner_id,
-//                                     vendor: resulte[0].vendor,
-//                                     bill_date: resulte[0].bill_date,
-//                                     due_date: resulte[0].due_date,
-//                                     amount_due: resulte[0].amount_due,
-//                                     categories: cat
-                            
-//                                 })
-//                             }
-//                             else{
-//                                 res.status(400).json({error: "Cannot fetch other users bill"});
-//                             }
-                          
-  
-//                         });
-//                     }
-//                     else{
-//                         res.send(400).json({error: "User does not exist"});
-//                     }
-//                 });
-//             }
-//         });
-//     }
-// });
+//GET ALL BILLS
 
 router.get("/",function(req,res){
 
@@ -314,7 +272,7 @@ router.get("/",function(req,res){
 
                 bcrypt.compare(password1, pa, (error, result) => {
                     if(result==true){
-                        //db.query(`Select id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount, categories from Bill where id = "${id}"`,function (error,resulte,rows,fields){
+                        //db.query(`Select id, created_ts, updated_ts, owner_id, vendor, bill_date, due_date, amount_due, categories from Bill where id = "${id}"`,function (error,resulte,rows,fields){
                             db.query(`Select * from Bill where owner_id = "${uuid}"`,function (error,resulte,rows,fields){  
                             if(error){
                                 throw error;
@@ -329,31 +287,39 @@ router.get("/",function(req,res){
                             //         vendor: resulte.vendor,
                             //         bill_date: resulte.bill_date,
                             //         due_date: resulte.due_date,
-                            //         amount_due: resulte.amount_due,
+                            //         amount_due_due: resulte.amount_due_due,
                             //         categories: cat
                             
                             //     })
                             // }
                             else{
-                                var resultJson = JSON.stringify(resulte);
-                                console.log(resulte);
-                                console.log(resulte.rows);
-                                console.log(resultJson);
+                                //var resultJson = JSON.stringify(resulte);
+                                //console.log(resulte);
+                                var jsonobj = resulte;
+                                console.log(jsonobj);
+                                jsonobj2 = JSON.stringify(jsonobj);
+
+                               // console.log(resulte.rows);
+                                //console.log(resultJson);
+                                
+                            
                                 res.status(200).json({message:"all values",
                                 
 
-                                            data : resulte
+                                            data : jsonobj2
                                             // created_ts: resulte.created_ts,
                                             // updated_ts: resulte.updated_ts,
                                             // owner_id: resulte.owner_id,
                                             // vendor: resulte.vendor,
                                             // bill_date: resulte.bill_date,
                                             // due_date: resulte.due_date,
-                                            // amount_due: resulte.amount_due,
+                                            // amount_due_due: resulte.amount_due_due,
                                             // categories: resulte.categories
                                     
                                      
                             });
+
+                          
                             }
                           
   
@@ -451,7 +417,7 @@ router.delete("/:id",(req,res)=>{
 })
 
 
-//put2
+//PUT
 
 router.put("/:id",function(req,res){
 
@@ -525,14 +491,28 @@ var updated_ts = timestamp;
                             else if(results[0].id != ress[0].owner_id){
                                     res.status(400).json({message : "You cannot update other users bill"});
                             }
+                            else if(Number(bill.amount_due< 0.01)|| (Number(bill.amount_due > 20000))){
+                                res.status(400).json({error: "The amount due is either less than minimum amount or greater than the maximum amount"});
+                            }
+                            else if(Number(bill.amount_due === undefined)){
+                                res.status(400).json({error: "amount due empty"});
+                               }
+                               else if(bill.paymentStatus === undefined){
+                                res.status(400).json({error: "paymentStatus empty"});
+                               }
+                               else if(!bill.vendor || !bill.bill_date || !bill.due_date || !bill.amount_due || !bill.categories || !bill.paymentStatus){
+                                res.status(400).json({error: "Please enter required fields"});
+                            }
+
                             else if(ress.length>0){
-                                db.query("UPDATE Bill SET  updated_ts='"+updated_ts+"',vendor='"+bill.vendor+"',bill_date='"+bill.bill_date+"',due_date='"+bill.due_date+"',amount='"+bill.amount_due+"',categories='"+stringObj+"',paymentStatus='"+bill.paymentStatus+"' where id = '"+id+"'",function (error,resulte){
+                                db.query("UPDATE Bill SET  updated_ts='"+updated_ts+"',vendor='"+bill.vendor+"',bill_date='"+bill.bill_date+"',due_date='"+bill.due_date+"',amount_due='"+bill.amount_due+"',categories='"+stringObj+"',paymentStatus='"+bill.paymentStatus+"' where id = '"+id+"'",function (error,resulte){
                                     //var ownerid = ress[0].owner_id;
                             
                                     if(error){
                                         throw error;
                                     }
                                     
+                                     
                                 
                                     else{
                                         res.status(200).json({
@@ -543,7 +523,7 @@ var updated_ts = timestamp;
                                             Vendor : bill.vendor,
                                             bill_date : bill.bill_date,
                                             Due_date : bill.due_date,
-                                            Amount : bill.amount_due,
+                                            amount_due : bill.amount_due,
                                             Categories : bill.categories,
                                             paymentStatus : bill.paymentStatus,
                                             message: "Updated"});
