9358e1797467ce930e2ce8b0de91a6251ab77226
sharwarigothe <gothe.s@northeastern.edu>
2020-03-07 13:35:14 -0800

    adding changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,24 +1,8 @@
   
 #!/bin/bash
 
-#sudo systemctl stop tomcat.service
-
-#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
-
-#sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-
-# cleanup log files
-#sudo rm -rf /opt/tomcat/logs/catalina*
-#sudo rm -rf /opt/tomcat/logs/*.log
-#sudo rm -rf /opt/tomcat/logs/*.txt
 cd home/ubuntu
-
 sudo npm install
-#sudo npm install forever -g
+sudo npm install aws-sdk
+sudo npm install multer-s3
 sudo npm install pm2@2.4 -g
-#sudo npm install dotenv --save
-
-#cd home/centos
-#forever stopall
-#pm2 stop all
-#sudo npm install pm2@2.4.0 -g
--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -4,6 +4,4 @@
 cd home/ubuntu
 ls --all
 ls --all
-sudo npm install aws-sdk
-sudo npm install multer-s3
 pm2 start server.js -f
