17ba571798701a38dbc92eeb264f33bf12489759
sharwarigothe <gothe.s@northeastern.edu>
2020-03-07 22:00:43 -0800

    changes

--- codedeploy/afterInstall.sh
+++
@@ -1,8 +0,0 @@
-  
-#!/bin/bash
-
-cd home/ubuntu
-pwd
-sudo npm install
-#sudo npm install pm2@2.4 -g
-sudo npm install forever -g
--- codedeploy/applicationStop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd home/ubuntu
-pwd
-#pm2 stop all
-forever stopall
