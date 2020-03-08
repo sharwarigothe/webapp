1a62aa9f75a6793606b13c67674765c165f0be8d
sharwarigothe <gothe.s@northeastern.edu>
2020-03-07 22:07:35 -0800

    changes

---
+++ codedeploy/applicationStart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd home/ubuntu
+ls --all
+ls --all
+#forever start server.js
+pm2 start server.js -f
+#pm2 server.js
+
+#!/bin/bash
+# cd home/ubuntu
+# ls --all
+# forever start server.js
+
---
+++ codedeploy/applicationStop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd home/ubuntu
+pwd
+pm2 stop all
+#forever stopall
