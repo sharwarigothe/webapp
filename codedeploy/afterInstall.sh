b9feb5d064d0fde6e1115f05c27d2366136cea8e
sharwarigothe <gothe.s@northeastern.edu>
2020-03-08 15:24:08 -0700

    pata nahi

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd home/ubuntu
+cd home/ubuntu/webapp
 pwd
 sudo npm install
 sudo npm install pm2@2.4 -g
--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd home/ubuntu
+cd home/ubuntu/webapp
 pwd
 pm2 stop all
