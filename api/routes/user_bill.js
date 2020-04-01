7932f07a77dafb5d01de5e524de1ccf1e61e0fc3
sharwarigothe <gothe.s@northeastern.edu>
2020-04-01 13:34:03 -0400

    ihgi

--- api/routes/user_bill.js
+++ api/routes/user_bill.js
@@ -417,13 +417,13 @@ router.get("/due/:x",(req,res)=>{
                         var today = new Date();
                         var newdate = new Date();
                         newdate.setDate(today.getDate() + 10);
-                        
+                        newdate1=newdate.toISOString();
 
                         logger.info("today: "+today);
                         logger.info("newdate: "+newdate);
                       //  logger.info("newdate1:"+newdate1);
                         
-                        db.query(`Select * from Bill where owner_id = "${uuid}" AND due_date < '${newdate}'`,function (error,resultdate,rows,fields){
+                        db.query(`Select * from Bill where owner_id = "${uuid}" AND due_date < '${newdate1}'`,function (error,resultdate,rows,fields){
                             //logger.info("due date: "+resultdate[0].due_date);
                             //var a = resultdate.length;
                             //var datanew=[];
