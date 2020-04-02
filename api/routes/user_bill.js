9cc734362d272b0d4b460f7c455f619226605c61
sharwarigothe <gothe.s@northeastern.edu>
2020-04-01 20:49:05 -0400

    email

--- api/routes/user_bill.js
+++ api/routes/user_bill.js
@@ -509,7 +509,7 @@ router.get("/due/:x",(req,res)=>{
                                         logger.info("entered 1st else");
                                         for (var i = 0; i<a; i++) {
                                             logger.info("entered else loop");
-                                            billLinks[i] = 'https://dev.sharwarigothe.me/v1/bill/'+resultsemail[i].id;
+                                            billLinks[i] = 'http://dev.sharwarigothe.me/v1/bill/'+resultdate[i].id;
                                             logger.info(billLinks[i]+"alalalalalal");
                                         }
                                         let sourceEmail = 'csye6225@dev.sharwarigothe.me';
